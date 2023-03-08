9595c8e01b28c2e842695dd1cab51bb26abea665
João Pedro Carvalho Xavier <TRI0106035@aluno.firjansenaisesi.com.br>
2023-03-07 21:09:28 -0300

    07-03-2023

---
+++ src/app.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+
+@Injectable()
+export class AppService {
+
+  private readonly users: User[]= [];
+
+  getHello(): string {
+    return 'Arquivos enviados';
+  }
+  createUser(user:User):User[]{
+    this.users.push(user);
+    return this.users;
+  }
+  getUsers(): User[]{
+    return this.users;
+  }
+}
